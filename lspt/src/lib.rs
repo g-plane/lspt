0d042a095aadda6f492489cdb46ae2ea774cbaea
Pig Fang <g-plane@hotmail.com>
2025-02-10 16:00:01 +0800

    add `HashMap` and `Uri`

--- lspt/src/lib.rs
+++ lspt/src/lib.rs
@@ -6,3 +6,17 @@ pub use enums::*;
 mod enums;
 
 pub type LSPAny = serde_json::Value;
+
+#[cfg(all(feature = "indexmap", feature = "rustc-hash"))]
+compile_error!("only one hash map implementation can be selected at the same time");
+#[cfg(all(feature = "indexmap", not(feature = "rustc-hash")))]
+pub type HashMap<K, V> = indexmap::IndexMap<K, V>;
+#[cfg(all(feature = "rustc-hash", not(feature = "indexmap")))]
+pub type HashMap<K, V> = rustc_hash::FxHashMap<K, V>;
+#[cfg(all(not(feature = "indexmap"), not(feature = "rustc-hash")))]
+pub type HashMap<K, V> = std::collections::HashMap<K, V>;
+
+#[cfg(feature = "url")]
+pub type Uri = url::Url;
+#[cfg(not(feature = "url"))]
+pub type Uri = String;
